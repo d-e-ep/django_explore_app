654893e3f1fbf7d1fd3aa3ae28cf321a03f19396
deepak sharma <deepakip82@gmail.com>
2021-09-23 21:12:06 +0530

    first commit

--- movies/apps.py
+++ movies/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class ExploreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'explore'
+
+class MoviesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'movies'
